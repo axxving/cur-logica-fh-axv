1e11aa98d0bc0a2e2fc6c5f5bdff817b13de9dbf
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-06-23 13:09:06 -0600

    feat(): using usestate
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,23 +1,32 @@
+import { useState } from "react";
 import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
 
 export const App = () => {
+
+  const [attemps, setAttemps] = useState(0);
+
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+    // Aumenta el numero de intentos y mantiene el numero en 9
+    setAttemps( Math.min( attemps + 1, 9 ) );
+  }
+
   return (
     <div className="app">
       {/** Imagen del juego */}
-      <HangImage imageNumber={ 5 } />
+      <HangImage imageNumber={ attemps } />
 
       {/** Palabra oculta */}
       <h3>-----------</h3>
 
       {/** Contador de intentos */}
-      <h2>Intentos: 0</h2>
+      <h2>Intentos: {attemps}</h2>
 
       {/** Botones de letras */}
       {letters.map((letter) => {
-        return <button key={letter}>{letter}</button>;
+        return <button onClick={() => checkLetter(letter)} key={letter}>{letter}</button>;
       })}
-      <button>a</button>
     </div>
   );
 };
